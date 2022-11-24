43960b3cf5091eec4eb12f579415d6394b946997
Joe Vermander <vermander1@protonmail.com>
2022-11-24 14:43:59 -0600

    update tetro position more efficiently

--- src/components/game/FallingTile.tsx
+++ src/components/game/FallingTile.tsx
@@ -1,38 +1,49 @@
 import React, { useState, useEffect, useRef, LegacyRef } from 'react';
 import { TileType, Coordinate } from 'data/types';
 import 'styles/Tile.css';
-import { Tetris } from 'data/Tetris';
 
 type props = {
   type: TileType;
-  coord: Coordinate;
+  pos: Coordinate; 
+  id: number;
 }
 
-const FallingTile: React.FC<props> = ({ type, coord }) => {
+// responsible for animating a tile in the current falling tetromino
+const FallingTile: React.FC<props> = ({ type, pos, id }) => {
   const ref = useRef<HTMLDivElement>();
+  const [origin, setOrigin] = useState<Coordinate | null>(null);
 
   useEffect(() => {
-    if(!ref.current)
+    var jsx = document.getElementById(`f${id}`);
+    if(!ref.current || !jsx)
       return;
-    ref.current.style.left = `calc(${coord.x}* var(--tileWidth))`;
-    ref.current.style.top = `calc((${coord.y} - 3)  * var(--tileHeight))`;
-    if(coord.y < 3)
-      ref.current.style.opacity = '0';
-    else
-      ref.current.style.opacity = '1';
-  }, [coord])
+
+    if(origin) {
+      ref.current.style.transform = 
+        `translate(calc( ${pos.x - origin.x} * var(--tileWidth) ), 
+                   calc( ${pos.y - origin.y} * var(--tileHeight) )
+         )`;
+      if(pos.y < 3) {
+        ref.current.style.opacity = '0';
+      } else {
+        ref.current.style.opacity = '1';
+      }
+    } else {
+      ref.current.style.left = `calc(${pos.x} * var(--tileWidth))`;
+      ref.current.style.top = `calc(${pos.y - 3} * var(--tileHeight))`;
+      setOrigin(pos);
+    }
+
+  }, [pos])
 
   return (
     <div
+      id={`f${id}`}
       ref={ref as LegacyRef<HTMLDivElement>}
       className={`falling-tile ${type}`}
-      style={{transition: 'left 0.15s, top 0.15s, opacity 0.15s', display: 'block'}}
     >
     </div>
   )
-
 }
-
-
 
 export default FallingTile;
